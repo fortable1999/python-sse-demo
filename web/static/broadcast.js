1904f90f580982888e9e790003a33c8a514c1bbb
fortable1999 <fortable1999@gmail.com>
2018-08-27 14:31:26 +0900

    add css for stream page

--- web/static/broadcast.js
+++ web/static/broadcast.js
@@ -1,15 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var urlParams = new URLSearchParams(window.location.search);
-    var topics = urlParams.get('topics');
+function listening(topics) {
     if (topics) {
         var es = new EventSource('/sse?topics='+topics);
     } else {
         var es = new EventSource('/sse');
     }
+	es.onerror = function () {
+		console.log('event error. retry.')
+		es.close()
+		listening(topics)
+	}
     es.onmessage = function (event) {
         var do_scroll = (window.innerHeight + window.pageYOffset) >= document.body.offsetHeight
         var messages_dom = document.getElementById('messages');
-        var message_dom = document.createElement('p');
+        var message_dom = document.createElement('li');
         var content_dom = document.createTextNode(event.data);
         message_dom.appendChild(content_dom);
         messages_dom.appendChild(message_dom);
@@ -18,5 +21,11 @@ document.addEventListener('DOMContentLoaded', function() {
             window.scrollTo(0,document.body.scrollHeight)
         }
     };
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var urlParams = new URLSearchParams(window.location.search);
+    var topics = urlParams.get('topics');
+	listening(topics);
 });
 
