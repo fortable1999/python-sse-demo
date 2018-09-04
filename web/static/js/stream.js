01737abc44a68bcb7a3c2fcab3f6248082ece068
Maeda, Shohei a | CPD <ar-shohei.a.maeda@rakuten.com>
2018-09-04 15:59:00 +0900

    Adjusted list size

--- web/static/js/stream.js
+++ web/static/js/stream.js
@@ -14,14 +14,14 @@ function listening(topics) {
         var do_scroll = (messages_dom.scrollTop + messages_dom.offsetHeight) >= messages_dom.scrollHeight
         var message_dom = document.createElement('li');
         var content_dom = document.createTextNode(event.data);
-	var message_count = messages_dom.getElementsByTagName('li').length;
+		var message_count = messages_dom.getElementsByTagName('li').length;
         message_dom.appendChild(content_dom);
         messages_dom.appendChild(message_dom);
-	console.log(message_count);
+		console.log(message_count);
         // message_dom.scrollIntoView();
-	if (message_count > 100){
-	messages_dom.removeChild(messages_dom.firstChild)
-	}
+		if (message_count > 100){
+			messages_dom.removeChild(messages_dom.firstChild)
+		}
 	
         if (do_scroll) {
             messages_dom.scrollTop = messages_dom.scrollHeight
